f7afcbfa889b43864c88ce8812700692c6578fec
Clément Dumas <dumasclement2002@gmail.com>
2023-05-06 03:28:45 +0200

    added constants

---
+++ src/constants.rs
@@ -0,0 +1,3 @@
+pub(crate) const MAX_SIZE: i64 = 2; // TODO: Switch to more reasonable value
+pub(crate) const MAX_SPEED: f64 = 10.;
+pub(crate) const MAX_FOV: i64 = 2; // TODO: Switch to more reasonable value
