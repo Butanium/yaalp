b4306cfd4edde55e0372f22aa01ae68d2bc66f2b
Clément Dumas <dumasclement2002@gmail.com>
2023-06-08 17:45:59 +0200

    added sprite constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,8 @@
+// Yaal specs:
 pub const MAX_SIZE: f32 = 30.;
 pub const MIN_SIZE: f32 = 5.;
 pub const MAX_SPEED: f32 = 10.;
 pub const MAX_FOV: i64 = 30;
 // TODO: balance this
+pub const YAAL_SPRITE: &str = "square.png";
+pub const BACKGROUND_SPRITE: &str = "grass.png";
