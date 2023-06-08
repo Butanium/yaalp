d08ca82d329bb9346d771187cb487eb1b6249674
Clément Dumas <dumasclement2002@gmail.com>
2023-06-08 14:52:19 +0200

    better constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,5 @@
-pub(crate) const MAX_SIZE: i64 = 2; // TODO: Switch to more reasonable value
-pub(crate) const MAX_SPEED: f64 = 10.;
-pub(crate) const MAX_FOV: i64 = 2; // TODO: Switch to more reasonable value
+pub const MAX_SIZE: f32 = 30.;
+pub const MIN_SIZE: f32 = 5.;
+pub const MAX_SPEED: f32 = 10.;
+pub const MAX_FOV: i64 = 30;
+// TODO: balance this
